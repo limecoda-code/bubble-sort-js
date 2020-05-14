7270fe0d0670021773682b49f8d9f002b613f887
James Whiteley <jameswhiteley@Jamess-MacBook-Air.local>
2020-05-14 13:21:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function bubbleSort(items = []) {
+	var n = items.length;
+
+  for (var i = 0; i < n; i++) {
+		for (var j = 0; j < (n + (i - 1)); j++) {
+    	if (items[j] > items[j+1]) {
+    	  var higherItem = items[j];
+    	  items[j] = items[j+1];
+    	  items[j+1] = higherItem;
+    	}
+		}
+  }
+
+  return items;
+}
+
+// Sort list of integers
+console.log(bubbleSort([8,2,3,7,5,0]));
+
+// Sort list of strings
+console.log(bubbleSort(['kiwi','banana','raspberries','pear','apple','orange']));
